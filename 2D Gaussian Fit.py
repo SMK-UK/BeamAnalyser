f86eda86fb8b76c83ead08c98e8544fa7602e749
Sean <keenan.sean@hotmail.co.uk>
2020-07-06 17:09:37 +0100

    Update

--- 2D Gaussian Fit.py
+++ 2D Gaussian Fit.py
@@ -5,31 +5,33 @@ Gaussian Beam Profile Extraction
 '''
 
 # import relevant modules
-
 import numpy as np
 import matplotlib.pyplot as mp
 from scipy import optimize
 
 # test data set
 # generate random gaussian with noise
+def gauss_1d(height, centre, width):
+    '''Generates Gaussian with given parameters'''
+    return lambda x: height * np.exp(-(np.power(x - centre, 2) / (2 * width ** 2)))
 
 def gauss_2d(height, centre_x, centre_y, width_x, width_y):
-    '''Generates 2D Gaussian with given parameters'''
+    '''Generates Gaussian with given parameters'''
     return lambda x, y : height*np.exp(-(np.power(x - centre_x, 2)/(2 * width_x ** 2) + np.power(y - centre_y, 2) / (2 * width_y ** 2)))
 
 # amplitude
 height = np.random.randint(low=0, high=20, size=1)
 # centre point x
-centre_x = np.random.randint(low=120, high=180, size=1)
+centre_x = np.random.randint(low=1, high=5, size=1)
 # centre point y
-centre_y = np.random.randint(low=120, high=180, size=1)
+centre_y = np.random.randint(low=1, high=5, size=1)
 # width in x
 width_x = np.random.randint(low=50, high=80, size=1)
 # width in y
 width_y = np.random.randint(low=50, high=80, size=1)
 
 # simulate pixels and generate gaussian
-x = np.linspace(start=0, stop=255, num=500)
+x = np.linspace(start=0, stop=501, num=500)
 y = x.copy()
 x, y = np.meshgrid(x, y)
 z = gauss_2d(height, centre_x, centre_y, width_x, width_y)(x, y)
@@ -49,20 +51,21 @@ mp.show()
 '''
 
 mp.matshow(z, cmap=mp.cm.gist_earth_r)
-# can fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
 
+# can fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
 def moments(data):
     '''Calculates parameters of a 2D gaussian function by calculating its moments (height, x, y, centre_x, centre_y width_x, width_y'''
     total = data.sum()
     X, Y = np.indices(data.shape)
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
+    centre_x = (X*data).sum()/total
+    centre_y = (Y*data).sum()/total
     height = data.max()
-    col = data[:, int(y)]
-    width_x = np.sqrt(np.abs((np.arange(col.size) - x) ** 2 * col).sum() / col.sum())
-    row = data[:, int(x)]
-    width_y = np.sqrt(np.abs((np.arange(col.size) - y) ** 2 * row).sum() / row.sum())
-    return height, x, y, width_x, width_y
+    # extract entire column from data of y
+    col = data[:, int(centre_y)]
+    width_x = np.sqrt(np.abs((np.arange(col.size) - centre_x) ** 2 * col).sum() / col.sum())
+    row = data[:, int(centre_x)]
+    width_y = np.sqrt(np.abs((np.arange(col.size) - centre_y) ** 2 * row).sum() / row.sum())
+    return height, centre_x, centre_y, width_x, width_y
 
 def fitgauss_2d(data):
     '''Returns 2D Gaussian parameters from fit (height, x, y, width_x, width_y'''
@@ -71,8 +74,12 @@ def fitgauss_2d(data):
     p, success = optimize.leastsq(err_fun, params)
     return p
 
+
 params = fitgauss_2d(z)
 fit = gauss_2d(*params)
 
 mp.contour(fit(*np.indices(z.shape)), cmap=mp.cm.copper)
 ax = mp.gca()
+
+x_params = np.delete(params, [2, 4])
+y_params = np.delete(params, [1, 3])
--- GaussFit.py
+++ GaussFit.py
@@ -5,8 +5,9 @@ Gaussian Beam Profile Extraction
 '''
 
 # import relevant modules
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
 from natsort import natsorted
 from PIL import Image
 import matplotlib.pyplot as mp
@@ -16,39 +17,73 @@ from scipy import optimize
 # interpolate data to generate 3D model of beam profile
 # save and print data to excel sheet
 
-'''Set-up Image file names and processing information'''
+''' Create functions to fit data '''
+
+# generate 2D gaussian
+def gauss_2d(height, centre_x, centre_y, width_x, width_y):
+    ''' Generates 2D Gaussian with given parameters:
+        height, centre_x, centre_y, width_x, width_y '''
+    return lambda x, y : height*np.exp(-(np.power(x - centre_x, 2)/(2 * width_x ** 2) + np.power(y - centre_y, 2) / (2 * width_y ** 2)))
+
+# fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
+def moments(data):
+    '''Calculates parameters of a 2D gaussian function by calculating its moments (height, x, y, centre_x, centre_y width_x, width_y'''
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    centre_x = (X*data).sum()/total
+    centre_y = (Y*data).sum()/total
+    height = data.max()
+    # extract entire column from data of y
+    col = data[:, int(centre_y)]
+    width_x = np.sqrt(np.abs((np.arange(col.size) - centre_x) ** 2 * col).sum() / col.sum())
+    row = data[:, int(centre_x)]
+    width_y = np.sqrt(np.abs((np.arange(col.size) - centre_y) ** 2 * row).sum() / row.sum())
+    return height, centre_x, centre_y, width_x, width_y
+
+def fitgauss_2d(data):
+    '''Returns 2D Gaussian parameters from fit (height, x, y, width_x, width_y'''
+    params = moments(data)
+    err_fun = lambda p: np.ravel(gauss_2d(*p)(*np.indices(data.shape)) - data)
+    p, success = optimize.leastsq(err_fun, params)
+    return p
+
+''' Set-up Image file names and processing information '''
 
 # directory name for images
 path = '/Users/Message/Desktop/CCD data/20cm lens/'
 file_list = os.listdir(path)
 # extract relevant files and sort
 image_list = natsorted([i for i in file_list if i.endswith('.bmp')])
+xl_file = [i for i in file_list if i.endswith('.xls')]
 # determine array dimensions for new data
 img = Image.open(path + image_list[0])
 imsize = img.size
+# read image position (requires xlrd)
+xlsx = pd.ExcelFile(path + xl_file[0])
+test = pd.read_excel(xlsx, sheet_name='Sheet1')
+z_pos = pd.read_excel(xlsx, sheet_name='Sheet1', header=2, usecols=['Distance (mm)'])
 
-''' read image and subtract background data - wont work if uneven number of files in folder '''
+''' Read image and subtract background data - wont work if uneven number of files in folder '''
 
 # read image and subtract background - store in new array
 data = np.empty([int(len(image_list)/2), imsize[0], imsize[1]])
+params = np.empty([int(len(image_list)/2), 5])
+fit = np.empty([int(len(image_list)/2), 5])
 for index, image in enumerate(image_list):
     # break when half way through data
     if index < int(len(image_list)/2):
-        # read then discard image
-        img = np.transpose(np.asarray(Image.open(path + image_list[2 * index])))
-        bkd = np.transpose(np.asarray(Image.open((path + image_list[2 * index + 1]))))
+        # read then discard image (change to int32 as uint8 does not have -ve values)
+        img = np.int32(np.transpose(np.asarray(Image.open(path + image_list[2 * index]))))
+        bkd = np.int32(np.transpose(np.asarray(Image.open((path + image_list[2 * index + 1])))))
         # array of data to fit
-        data[index,:,:] = img - bkd
+        data[index, :, :] = np.absolute(img - bkd)
+        params[index, :] = fitgauss_2d(data[index,:,:])
+        fit[index, :] = gauss_2d(params[index, :])
     else:
         break
 
-''' test data set - generate dummy gaussian in 2D and then extract FWHM in x and y, plotting fit to data '''
 
-# generate random gaussian with noise
-def gauss_2d(height, centre_x, centre_y, width_x, width_y):
-    ''' Generates 2D Gaussian with given parameters:
-        height, centre_x, centre_y, width_x, width_y '''
-    return lambda x, y : height*np.exp(-(np.power(x - centre_x, 2)/(2 * width_x ** 2) + np.power(y - centre_y, 2) / (2 * width_y ** 2)))
+''' Test data set - generate dummy gaussian in 2D and then extract FWHM in x and y, plotting fit to data '''
 
 # generate pixel grid
 x = np.arange(start=0, stop=960, step=1)
