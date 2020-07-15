3e604529d76599d6170144781ded10974b832b27
Sean Keenan <67632803+naneek91@users.noreply.github.com>
2020-07-15 18:03:30 +0200

    Add files via upload

--- 2D Gaussian Fit.py
+++ 2D Gaussian Fit.py
@@ -5,74 +5,70 @@ Gaussian Beam Profile Extraction
 '''
 
 # import relevant modules
-
 import numpy as np
 import matplotlib.pyplot as mp
 from scipy import optimize
 
-# test data set
-# generate random gaussian with noise
+# define neccesary functions
+def gauss_1d(height, centre, width):
+    '''Generates Gaussian with given parameters'''
+    return lambda x: height * np.exp(-(np.power(x - centre, 2) / (2 * width ** 2)))
 
 def gauss_2d(height, centre_x, centre_y, width_x, width_y):
-    '''Generates 2D Gaussian with given parameters'''
+    '''Generates Gaussian with given parameters'''
     return lambda x, y : height*np.exp(-(np.power(x - centre_x, 2)/(2 * width_x ** 2) + np.power(y - centre_y, 2) / (2 * width_y ** 2)))
 
-# amplitude
-height = np.random.randint(low=0, high=20, size=1)
-# centre point x
-centre_x = np.random.randint(low=120, high=180, size=1)
-# centre point y
-centre_y = np.random.randint(low=120, high=180, size=1)
-# width in x
-width_x = np.random.randint(low=50, high=80, size=1)
-# width in y
-width_y = np.random.randint(low=50, high=80, size=1)
-
-# simulate pixels and generate gaussian
-x = np.linspace(start=0, stop=255, num=500)
-y = x.copy()
-x, y = np.meshgrid(x, y)
-z = gauss_2d(height, centre_x, centre_y, width_x, width_y)(x, y)
-
-# generate and add noise to data
-noise = 0.5 * np.random.normal(loc=0.5, scale=1, size=z.shape)
-z += noise
-
-'''
-# plot data
-fig_0, ax_0 = mp.subplots(1, 1)
-gauss_test = ax.contourf(x, y, z)
-ax_0.set_title(' Random Beam Profile ')
-ax_0.set(xlabel='Pixel', ylabel='Pixel')
-fig_0.colorbar(gauss_test)
-mp.show()
-'''
-
-mp.matshow(z, cmap=mp.cm.gist_earth_r)
 # can fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
-
 def moments(data):
     '''Calculates parameters of a 2D gaussian function by calculating its moments (height, x, y, centre_x, centre_y width_x, width_y'''
     total = data.sum()
     X, Y = np.indices(data.shape)
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
+    centre_x = (X*data).sum()/total
+    centre_y = (Y*data).sum()/total
     height = data.max()
-    col = data[:, int(y)]
-    width_x = np.sqrt(np.abs((np.arange(col.size) - x) ** 2 * col).sum() / col.sum())
-    row = data[:, int(x)]
-    width_y = np.sqrt(np.abs((np.arange(col.size) - y) ** 2 * row).sum() / row.sum())
-    return height, x, y, width_x, width_y
+    # extract entire column from data of y
+    col = data[int(centre_x), :]
+    width_x = np.sqrt(np.abs((np.arange(col.size) - centre_x) ** 2 * col).sum() / col.sum())
+    row = data[:, int(centre_y)]
+    width_y = np.sqrt(np.abs((np.arange(row.size) - centre_y) ** 2 * row).sum() / row.sum())
+    return height, centre_x, centre_y, width_x, width_y
 
 def fitgauss_2d(data):
     '''Returns 2D Gaussian parameters from fit (height, x, y, width_x, width_y'''
     params = moments(data)
     err_fun = lambda p: np.ravel(gauss_2d(*p)(*np.indices(data.shape)) - data)
-    p, success = optimize.leastsq(err_fun, params)
+    p, success = optimize.least_squares(err_fun, params)
     return p
 
+''' Test data set - generate dummy gaussian in 2D and then extract FWHM in x and y, plotting fit to data '''
+
+# generate pixel grid
+x = np.arange(start=0, stop=1280, step=1)
+y = np.arange(start=0, stop=960, step=1)
+x, y = np.meshgrid(x, y)
+
+# gaussian attributes
+# amplitude
+height = np.random.randint(low=90, high=120, size=1)
+# centre point x
+x_0 = int(np.round(np.max(x)/2))
+centre_x = np.random.randint(low=550 , high=700, size=1)
+# centre point y
+y_0 = int(np.round(np.max(y)/2))
+centre_y = np.random.randint(low=150, high=200, size=1)
+# width in x
+width_x = np.random.randint(low=35, high=40, size=1)
+# width in y
+width_y = np.random.randint(low=35, high=40, size=1)
+
+# generate gaussian and add noise to data
+z = gauss_2d(height, centre_x, centre_y, width_x, width_y)(x, y)
+noise = 0.5 * np.random.normal(loc=0.6, scale=1, size=z.shape)
+z += noise
+
+fig, ax = mp.subplots()
+ax.matshow(z, cmap=mp.cm.gist_earth_r)
 params = fitgauss_2d(z)
 fit = gauss_2d(*params)
+ax.contour(fit(*np.indices(z.shape)), cmap=mp.cm.copper)
 
-mp.contour(fit(*np.indices(z.shape)), cmap=mp.cm.copper)
-ax = mp.gca()
---
+++ Beam Fit.py
@@ -0,0 +1,120 @@
+'''
+Sean Keenan, 5th Year MPhys Heriot-Watt University, Edinburgh
+Mazerra group
+Gaussian Beam Profile Extraction
+'''
+
+# import relevant modules
+import os
+import numpy as np
+import pandas as pd
+from natsort import natsorted
+from PIL import Image
+import matplotlib.pyplot as mp
+from scipy.optimize import curve_fit
+
+# define neccesary functions
+def gaussian(x, height, centre, sigma):
+    '''Generates Gaussian with given parameters'''
+    return height * np.exp(-(np.power(x - centre, 2) / (2 * sigma ** 2)))
+
+# fit a gaussian to data by calculating its 'moments' (mean, variance, width, height)
+def moments(data):
+    '''Calculates parameters of a gaussian function by calculating
+    its moments (height, mean_x, width_x, mean_y, width_y'''
+    height = np.amax(data)
+    centre = np.where(data == height)
+    dim = np.size(centre)
+    # handle case where there are multiple 'max' values
+    if dim > 2:
+        centre = np.array(centre)
+        mean = np.sum(centre, 1)
+        mean_x = round(mean[0] / len(centre[0]))
+        mean_y = round(mean[1] / len(centre[1]))
+    else:
+        mean_x = int(centre[0])
+        mean_y = int(centre[1])
+
+    row = data[:, mean_y]
+    col = data[mean_x, :]
+    sigma_x = np.sqrt(((row - height) ** 2).sum() / len(row))
+    sigma_y = np.sqrt(((col - height) ** 2).sum() / len(col))
+    return height, mean_x, sigma_x, mean_y, sigma_y
+
+def fitgauss(data):
+    '''Returns seperate x-y Gaussian parameters from fit to 2D gaussian data
+     (height, mean_x, width_x, mean_y, width_y)'''
+    params = moments(data)
+    # extract data along index of maximum value
+    x = data[:, params[3]]
+    y = data[params[1], :]
+    # fit gaussian to data and return probability
+    fit_x, success_x = curve_fit(gaussian, np.arange(1, len(x) +1 ), x, p0=params[0:3])
+    fit_y, success_y = curve_fit(gaussian, np.arange(1, len(y) +1 ), y, p0=(params[0], params[3], params[4]))
+    x_err = np.sqrt(np.diag(success_x))
+    y_err = np.sqrt(np.diag(success_y))
+    # condense fit data into array for output
+    fit_data = np.array([fit_x, fit_y])
+    fit_err = np.array([x_err, y_err])
+    return fit_data, fit_err
+
+''' Set-up Image file names and processing information '''
+
+# directory name for images
+path = '/Users/Message/Desktop/CCD Data/40cm lens/'
+file_list = os.listdir(path)
+# extract relevant files and sort
+image_list = natsorted([i for i in file_list if i.endswith('.bmp')])
+xl_file = [i for i in file_list if i.endswith('.xls')]
+# determine array dimensions for new data
+img = Image.open(path + image_list[0])
+imsize = img.size
+# read image position (requires xlrd)
+xlsx = pd.ExcelFile(path + xl_file[0])
+df_orig = pd.read_excel(xlsx, sheet_name='Sheet1')
+# distance from lens (um)
+z_pos = pd.read_excel(xlsx, sheet_name='Sheet1', header=2, usecols=['Distance (mm)'])
+z = np.unique(z_pos.to_numpy()) * 1e3
+# chip and pixel size (um)
+chip_size = 1/3 * 25.4
+pix_size = 4.5 # chip_size / imsize[0]
+# image laser wavelength(um)
+wavelen = 606e-3
+
+''' Read image and subtract background data - wont work if uneven number of files in folder '''
+
+# read image and subtract background - store in new array
+data = np.empty([int(len(image_list)/2), imsize[0], imsize[1]])
+fit_data = np.empty([int(len(image_list)/2), 2, 3])
+fit_err = np.empty([int(len(image_list)/2), 2, 3])
+for index, image in enumerate(image_list):
+    # break when half way through data
+    if index < int(len(image_list)/2):
+        # read then discard image (change to int32 as uint8 does not have -ve values)
+        img = np.int32(np.transpose(np.asarray(Image.open(path + image_list[2 * index]))))
+        bkd = np.int32(np.transpose(np.asarray(Image.open((path + image_list[2 * index + 1])))))
+        # arrays of data, params and fit
+        data[index, :, :] = np.absolute(img - bkd)
+        fit_data[index, :, :], fit_err[index, :, :] = fitgauss(data[index,:,:])
+    else:
+        break
+
+# calculate FWHM (mm) & divergence (deg)
+FWHM = 2 * np.sqrt(2 * np.log(2)) * fit_data[:, :, 2] * pix_size
+theta_D = np.empty(FWHM.shape)
+for col in range(len(FWHM.shape)):
+    theta_D[:, col] = np.rad2deg(np.divide(FWHM[:, col], z))
+
+# calculate beam waist and rayleigh range (mm)
+w_0 = wavelen/ (np.pi*theta_D)
+z_R = (w_0 ** 2 * np.pi) / wavelen
+
+# append new data to original and write to file
+data_out = np.concatenate((w_0, z_R, theta_D), axis=1)
+col_name = ['X-waist (micron)', 'Y-waist (micron)', 'X-Zr (micron', 'Y-Zr (micron)', 'X-theta D (deg)', 'Y-theta D (deg)']
+df_out = pd.DataFrame(data=data_out,columns=col_name)
+''' df_new = pd.DataFrame.append(df_orig, df_out, ignore_index=True)
+
+with pd.ExcelWriter((path + xl_file[0]), engine='openpyxl', mode='w') as writer:
+    df_new.to_excel(writer, ignore_index=True)
+'''
